140eaa334196211bb32720e52383adc46e58adc5
ttaozhiming <483179429@qq.com>
2023-09-15 07:50:09 +0800

    Site updated: 2023-09-15 07:50:09

---
+++ static/font.js
@@ -0,0 +1,7 @@
+
+(function() {
+    const subtitle = document.getElementById('subtitle')
+    if (subtitle.dataset.typedText !== "taozhiming's blog") return
+    subtitle.classList.add('fancy-title')
+})();
+
